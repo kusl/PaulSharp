d87ef254f9e693a1ead83f47030bfa55857a041d
kushal <kushaldeveloper@gmail.com>
2015-07-17 16:54:53 -0400

    add threes and fives test

--- EulerSharp.Tests/ThreesAndFivesTest.cs
+++ EulerSharp.Tests/ThreesAndFivesTest.cs
@@ -4,7 +4,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace EulerSharp.Tests
 {
     [TestClass]
-    public class UnitTest1
+    public class ThreesAndFivesTest
     {
         [TestMethod]
         public void TestMethod1()
