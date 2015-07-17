ef864d7029b2dc38ccc822711fe26bc7f6794369
kushal <kushaldeveloper@gmail.com>
2015-07-17 17:18:43 -0400

    try to write test for summing up threes

--- EulerSharp.Tests/ThreesAndFivesTest.cs
+++ EulerSharp.Tests/ThreesAndFivesTest.cs
@@ -9,9 +9,17 @@ namespace EulerSharp.Tests
         [TestMethod]
         public void Test_find_sum()
         {
-            var input = 0;
-            var output = EulerSharp.ThreesAndFives.find_sum(input);
+            var input = 10;
+            var output = EulerSharp.ThreesAndFives.find_sum(below_integer: input);
             Assert.AreEqual(output, 0);
+        }
+        [TestMethod]
+        public void Test_find_sum_list()
+        {
+            var number_to_multiple = 3;
+            var below_integer = 10;
+            var output = EulerSharp.ThreesAndFives.sum_of_list(number_to_multiple: number_to_multiple, below_integer: below_integer);
+            Assert.AreEqual(output, 3 + 6 + 9);
         }
     }
 }
--- EulerSharp/ThreesAndFives.cs
+++ EulerSharp/ThreesAndFives.cs
@@ -7,7 +7,29 @@ namespace EulerSharp
 {
     public class ThreesAndFives
     {
-        public static int find_sum(int below_int)
+        private static System.Collections.Generic.List<int> list_of_multiples_of_number_below_int(int number_to_multiple, int below_integer)
+        {
+            var my_list = new System.Collections.Generic.List<int>();
+            for (int i = 0; i < below_integer; i++)
+            {
+                if (i % number_to_multiple == 0)
+                {
+                    my_list.Add(i);
+                }
+            }
+            return my_list;
+        }
+        public static int sum_of_list(int number_to_multiple, int below_integer)
+        {
+            System.Collections.Generic.List<int> input_list = list_of_multiples_of_number_below_int(number_to_multiple, below_integer);
+            var accumulator = 0;
+            foreach(int i in input_list)
+            {
+                accumulator += i;
+            }
+            return accumulator;
+        }
+        public static int find_sum(int below_integer)
         {
             return 0;
         }
